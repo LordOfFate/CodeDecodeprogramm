c663e97bd48f43090876118f70ec8f1fe4309efb
tolik241510 <tolik241510@gmail.com>
2022-04-05 09:59:41 +0300

    Make changes in decoder.

--- decoder.py
+++ decoder.py
@@ -3,14 +3,14 @@ class list:
     class node:
         element = None
         next_element = None
+        ZeroAdd = None
+        OneAdd = None
         oneAddElement = None
         zeroAddElement = None
 
-        def __init__(self, element, next_element = None, oneAddElement = None, zeroAddElement = None):
+        def __init__(self, element, next_element = None):
             self.element = element
             self.next_element = next_element
-            self.oneAddElement = oneAddElement
-            self.zeroAddElement = zeroAddElement
 
     head = node
 
@@ -32,7 +32,7 @@ class list:
             current = current.next_element
         print(current.element, ' |--> ', current.zeroAddElement, ' ', current.oneAddElement)
 
-    def addChain(self, element, ChainZeroElement, ChainOneElement):
+    def addChain(self, element, ChainZeroElement, ChainOneElement, ZeroAddElement, OneAddElement):
         current = self.head
         currentIter = self.head
         while current != element:
@@ -43,3 +43,5 @@ class list:
         while currentIter != ChainOneElement:
             curretIter = current.next_element
         current.oneAddElement = curretIter
+        current.ZeroAdd = ZeroAddElement
+        current.OneAdd = OneAddElemnt
